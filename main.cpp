9ad0879b4eead2c4d0a590211e1f7e155f7280f1
dmitriprilepski@gmail.com <dmitriprilepski@gmail.com>
2017-01-14 01:53:54 -0500

    initial parallel version

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <omp.h>
 
 int main(int argc, char **argv) {
+    
+  #pragma omp parallel
+  {
     int ID = 0;
     
     std::cout << "Hello " << ID << std::endl;
     std::cout << "World " << ID << std::endl;
+  }
     return 0;
 }
