3536bab740f9067e8170510ceaac14e161ddc860
dmitriprilepski@gmail.com <dmitriprilepski@gmail.com>
2017-01-29 17:15:53 -0500

    added schduled for case

--- pi/pi.cpp
+++ pi/pi.cpp
@@ -91,11 +91,35 @@ double pi_threaded_atomic() {
   std::cout << "Threaded Atomic PI Time: " << omp_get_wtime() - start_time << std::endl;  
 }
 
+// CAlculating PI in parallel with schedule
+double pi_for() {
+  double start_time = omp_get_wtime();
+  static long num_steps = 100000;
+  double step;
+
+  double x, pi, sum = 0.0;
+
+  step = 1.0 / (double) num_steps;
+
+  #pragma omp parallel for schedule(auto) reduction(+:sum)
+    for (long i = 0; i < num_steps; i++){
+      x = (i + 0.5) * step;
+      sum += 4.0 / (1.0 + x * x);
+    }
+
+  pi = step * sum;
+
+  std::cout << "Parallel For PI: " << pi << std::endl;
+  std::cout << "Parallel For Time: " << omp_get_wtime() - start_time << std::endl;  
+}
+
+
 int main(int argc, char **argv) {
 
   pi_sequencial();
   pi_threaded();
   pi_threaded_atomic();
+  pi_for();
 /*
   #pragma omp parallel
   {
